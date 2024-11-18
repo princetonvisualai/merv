1c27360eef5b7e5cafcf6c5ccb0e65f78b272219
a6o <18547345+a6o@users.noreply.github.com>
2024-11-17 22:42:00 -0500

    fixed gpt_batching

--- scripts/eval_gpt_batch.py
+++ scripts/eval_gpt_batch.py
@@ -1,68 +1,20 @@
 import argparse
 import ast
-import asyncio
 import json
 import os
-import random
 import time
-
 import openai
-import yaml
 from tqdm import tqdm
-
+import io
 
 def parse_args():
     parser = argparse.ArgumentParser(description="question-answer-generation-using-gpt-4o-mini")
     parser.add_argument("--ckpt-name", default=r"", help="Name of checkpoint.")
     parser.add_argument("--benchmark", default=r"", help="Benchmark.")
-    # parser.add_argument("--pred_path", default=r"", help="The path to file containing prediction.")
-    # parser.add_argument("--output_file", default=r"", help="The path to save annotation json files.")
     args = parser.parse_args()
     return args
 
-
-async def run(message, qa_set, completed_files, key, client, openai_modelname):
-    try:
-        completion = client.chat.completions.create(model=openai_modelname, messages=message, max_tokens=500)
-
-        response = await completion
-        response = response.dict()
-
-        response_message = response["choices"][0]["message"]["content"]
-        response_dict = ast.literal_eval(response_message)
-
-        assert isinstance(response_dict, dict)
-        assert "score" in response_dict and "pred" in response_dict
-        assert isinstance(response_dict["score"], int) or isinstance(response_dict["score"], float)
-        assert response_dict["pred"] in ["yes", "no"]
-        result_qa_pair = [response_dict, qa_set]
-
-        # Save the question-answer pairs to a json file.
-        completed_files[key] = result_qa_pair
-        # with open(saveloc, "w") as f:
-        #     json.dump(result_qa_pair, f)
-
-    except SyntaxError as e:
-        print(f"Error processing file '{key}': {e}")
-    except ValueError as e:
-        print(f"Error processing file '{key}': {e}")
-
-    except openai.BadRequestError as e:
-        print(f"Error processing file '{key}': {e}")
-
-        if "invalid_prompt" in e.code:
-            print(message)
-            return key
-        if "content_filter" in e.code:
-            print(message)
-            return key
-
-    except Exception as e:
-        print(type(e))
-        print(f"Error processing file '{key}': {e}")
-
-
-def query_gpt(prediction_set, caption_files, gpt_batch_input_path, gpt_batch_output_path):
+def query_gpt(client, prediction_set, caption_files, gpt_batch_batch_id, ckpt_name, benchmark):
     """
     Evaluates question and answer pairs using GPT-3
     Returns a score for correctness.
@@ -71,7 +23,8 @@ def query_gpt(prediction_set, caption_files, gpt_batch_input_path, gpt_batch_out
 
     openai_modelname = "gpt-4o-mini-2024-07-18"
 
-    for key in tqdm(caption_files):
+    jsonl = []
+    for key in caption_files:
         qa_set = prediction_set[key]
         question = qa_set["q"]
         answer = qa_set["a"]
@@ -111,39 +64,151 @@ def query_gpt(prediction_set, caption_files, gpt_batch_input_path, gpt_batch_out
                 "max_tokens": 500,
             },
         }
-        with open(gpt_batch_input_path, "a") as f:
-            f.write(json.dumps(payload) + "\n")
 
-    client = openai.OpenAI(api_key=OPENAI_KEY)
-    batch_input_file = client.files.create(
-        file=open(gpt_batch_input_path, "rb"),
-        purpose="batch",
-    ) 
+        jsonl.append(json.dumps(payload))
 
-    batch_input_file_id = batch_input_file.id
+    #jsonl cannot have more than 50K items, and 200M in size
+    batchfile = []
+    size = 0
+    ind = 0
+    batch_input_files = []
+    for l in jsonl:
+        this_size = len(l)/1024/1024
+        if size  + this_size > 190 or len(batchfile) > 3:
+            fileio = io.BytesIO("\n".join(batchfile).encode('utf-8'))
+            fileio.name = '{}_{}_{}.jsonl'.format(ckpt_name, benchmark, ind)
+            batch_input_file = client.files.create(
+                file=fileio,
+                purpose="batch"
+            )
+            batch_input_files.append(batch_input_file)
 
-    submitted_batch = client.batches.create(
-        input_file_id=batch_input_file_id,
-        endpoint="/v1/chat/completions",
-        completion_window="24h",
-        metadata={
-            "description": "nightly eval job"
-        }
-    )
+            batchfile = [l]
+            size = this_size
+            ind += 1
+        else:
+            batchfile += [l]
+            size += this_size
+    
+    if len(batchfile) > 0:
+        
+        fileio = io.BytesIO("\n".join(batchfile).encode('utf-8'))
+        fileio.name = '{}_{}_{}.jsonl'.format(ckpt_name, benchmark, ind)
+        batch_input_file = client.files.create(
+            file=fileio,
+            purpose="batch"
+        )
+        batch_input_files.append(batch_input_file)
+        ind+=1
 
-    return submitted_batch
+    # we should check if file status is 'precessed' but its now deprecated... hmm
+    submitted_batchs = []
+    for batch_input_file in batch_input_files:
+        submitted_batch = client.batches.create(
+            input_file_id=batch_input_file.id,
+            endpoint="/v1/chat/completions",
+            completion_window="24h",
+            metadata={
+                "description": "nightly eval job"
+            }
+        )
+        submitted_batchs.append(submitted_batch)
 
+    with open(gpt_batch_batch_id, 'w') as f:
+        for submitted_batch in submitted_batchs:
+            f.write(json.dumps(submitted_batch.json())+"\n")
+
+    return submitted_batchs
+        
+def save_evaluation_result(jsonl, prediction_set, gpt_batch_output_path):
+
+    gpt_outputs = {}
+    for line in jsonl.split("\n"):
+        if len(line) == 0:
+            continue
+
+        linej = json.loads(line)
+
+        try:
+            prediction = ast.literal_eval(linej['response']['body']['choices'][0]['message']['content'])
+            assert 'pred' in prediction and 'score' in prediction
+        except:
+            prediction = {'pred': 'no', 'score': 0}
+        gpt_outputs[linej['custom_id']] = prediction
+
+    with open(gpt_batch_output_path, "w") as f:
+        gpt_outputs_with_missing = {}
+        for k, v in prediction_set.items():
+            if k in gpt_outputs:
+                gpt_outputs_with_missing[k] = ([gpt_outputs[k], v])
+            else:
+                gpt_outputs_with_missing[k] = ([{'pred': 'no', 'score': 0}, v])
+
+        json.dump(gpt_outputs_with_missing, f)
+   
+def print_calculated_performance(gpt_batch_output_path):
+
+    with open(gpt_batch_output_path) as f:
+        gpt_outputs = json.load(f)
+
+    score_sum = 0
+    count = 0
+    yes_count = 0
+    no_count = 0
+
+    for k, v in gpt_outputs.items():
+        count += 1
+        score_sum += v[0]['score']
+        if 'yes' in v[0]['pred'].lower():
+            yes_count += 1
+        else:
+            no_count += 1
+
+    average_score = score_sum / count
+    accuracy = yes_count / (yes_count + no_count)
+    print("Yes count:", yes_count)
+    print("No count:", no_count)
+    print("Accuracy:", accuracy)
+    print("Average score:", average_score)
+
+    return accuracy, average_score
+
+def print_token_usage(jsonl):
+    """
+    Prints token usage of the OpenAI API
+    """
+    prompt_tokens = 0
+    completion_tokens = 0
+    for line in jsonl.split("\n"):
+
+        if len(line) == 0:
+            continue
+
+        linej = json.loads(line)
+
+        prompt_tokens += linej['response']['body']['usage']['prompt_tokens']
+        completion_tokens += linej['response']['body']['usage']['completion_tokens']
+
+    print(f"Prompt tokens: {prompt_tokens}, Completion tokens: {completion_tokens}")
+    if linej['response']['body']['model'] == 'gpt-4o-mini-2024-07-18':
+        print("Total Cost on {}".format(linej['response']['body']['model']))
+        print('${:.10f}'.format(0.075*prompt_tokens/1000000 + 0.3 * completion_tokens/1000000))
+    else:
+        print('IDK cost for this model')
+    print(f"")
 
 def main():
-    """
-    Main function to control the flow of the program.
-    """
-    # Parse arguments.
     args = parse_args()
 
     pred_path = f"eval_result/{args.ckpt_name}/{args.benchmark}_pred_merge.jsonl"
-    gpt_batch_input_path = f"eval_result/{args.ckpt_name}/{args.benchmark}_gpt_batch_input.jsonl"
-    gpt_batch_output_path = f"eval_result/{args.ckpt_name}/{args.benchmark}_gpt_return.jsonl"
+    gpt_batch_batch_id = f"eval_result/{args.ckpt_name}/{args.benchmark}_gpt_batch_id.jsonl"
+    gpt_batch_return_path = f"eval_result/{args.ckpt_name}/{args.benchmark}_gpt_return.jsonl"
+    gpt_batch_output_path = f"eval_result/{args.ckpt_name}/{args.benchmark}_gpt.json"
+
+    if os.path.exists(gpt_batch_output_path):
+        print('File', gpt_batch_output_path, 'already exists. Evaluation was already done!')
+        print_calculated_performance(gpt_batch_output_path)
+        exit()
 
     # Set the OpenAI API key.
     with open(".openai_key") as f:
@@ -173,62 +238,44 @@ def main():
         qa_set = {"q": question, "a": answer, "pred": pred}
         prediction_set[q_id] = qa_set
 
-    ### Need to create a batch file at eval_result/{ckpt}/{benchmark}_batch.jsonl
-    submitted_batch = query_gpt(prediction_set, id_list, gpt_batch_input_path, gpt_batch_output_path)
+    
+    if not os.path.exists(gpt_batch_batch_id):
+        submitted_batchs = query_gpt(client, prediction_set, id_list, gpt_batch_batch_id, args.ckpt_name, args.benchmark)
+        print('We submitted the batch job. Please check back later.')
+    else:
+        print('Batch job already submitted. Checking if finetuning is finished.')
 
-    ### Monitor now for return of the batch
+    with open(gpt_batch_batch_id) as f:
+        submitted_batch_ids = []
+        with open(gpt_batch_batch_id, 'r') as f:
+
+            for line in f.readlines():
+                gpt_batch_id = json.loads(line.strip())
+                submitted_batch_ids.append(json.loads(gpt_batch_id)['id'])
+
+
     while True:
-        batch_retrieved = client.batches.retrieve(submitted_batch.id)
-        batch_status = batch_retrieved.status
-        print(f"Batch status: {batch_status}")
-        if batch_status == "completed":
+        batches_retrieved = [client.batches.retrieve(submitted_batch_id) for submitted_batch_id in submitted_batch_ids]
+        batches_status = [batch_retrieved.status for batch_retrieved in batches_retrieved]
+        print(f"Batches status: {batches_status}")
+        if all([batch_status == "completed" for batch_status in batches_status]):
             break 
-        elif batch_status in ["in_progress", "validating", "finalizing"]:
+        elif any([batch_status in ["in_progress", "validating", "finalizing"] for batch_status in batches_status]):
             print("sleeping for 60 seconds")
             time.sleep(60)
         else:
-            print("Batch failed")
+            print("Batch failed. They have to be either completed, in_progress, validating, or finalizing")
             exit()
-
-    file_response = client.files.content(batch_retrieved.output_file_id)
-    with open(gpt_batch_output_path, "wb") as f:
-        f.write(file_response)
     
-    # Combine all the processed files into one
-    combined_contents = []
-    with open(gpt_batch_output_path) as f:
-        for line in f:
-            combined_contents.append(json.loads(line))
+    file_responses = "\n".join([client.files.content(batch_retrieved.output_file_id).text for batch_retrieved in batches_retrieved])
+    file_responses = "\n".join([l for l in file_responses.split("\n") if len(l)!=0])
+    with open(gpt_batch_return_path, "w") as f:
+        f.write(file_responses)
 
-    score_sum = 0
-    count = 0
-    yes_count = 0
-    no_count = 0
-    for key, result in tqdm(combined_contents.items()):
-        # Computing score
-        count += 1
-        try:
-            score_match = result[0]["score"]
-        except:
-            print(result)
-            exit()
-        score = int(score_match)
-        score_sum += score
-
-        # Computing accuracy
-        pred = result[0]["pred"]
-        if "yes" in pred.lower():
-            yes_count += 1
-        elif "no" in pred.lower():
-            no_count += 1
-
-    average_score = score_sum / count
-    accuracy = yes_count / (yes_count + no_count)
-    print("Yes count:", yes_count)
-    print("No count:", no_count)
-    print("Accuracy:", accuracy)
-    print("Average score:", average_score)
-
+    print(gpt_batch_return_path)
+    print_token_usage(file_responses)
+    save_evaluation_result(file_responses, prediction_set, gpt_batch_output_path)
+    print_calculated_performance(gpt_batch_output_path)
 
 if __name__ == "__main__":
     main()
